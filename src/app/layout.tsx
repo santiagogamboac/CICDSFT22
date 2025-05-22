8aa296e7473cfd9dc83dcf74135a1eb23e96e8d4
santiagogamboac <santiagogamboacely@gmail.com>
2025-05-21 21:55:52 -0500

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "./components/header";
+
 import Footer from "./components/footer";
+import Header from "./components/header";
 const inter = Inter({ subsets: ["latin"] })
 
 
@@ -19,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-        <body className={inter.className}>
+      <body className={inter.className}>
         <div className="flex flex-col min-h-screen">
           <Header />
           <main className="flex-grow">{children}</main>
