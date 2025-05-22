92110aa922de2abb4fdc7ce008f4da9102d81816
santiagogamboac <santiagogamboacely@gmail.com>
2025-05-21 22:06:12 -0500

    Footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-import Footer from "./components/footer";
+
 import Header from "./components/header";
+import Footer from "./components/footer-component";
 const inter = Inter({ subsets: ["latin"] })
 
 
